c0bb34eb98c6f64eb781ba8e7cb20bdcb12cd29c
Nathan <ng125@mail.chapman.edu>
2014-10-14 00:10:27 -0700

    prototype works as previously, bulletML still not functional

--- spassss/Assets/Scripts/DestroyByContact.cs
+++ spassss/Assets/Scripts/DestroyByContact.cs
@@ -88,6 +88,7 @@ public class DestroyByContact : MonoBehaviour
 		if (other.tag == "Player") {
 			Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
 			gameController.GameOver ();
+			Destroy (gameObject);
 			Destroy(other.gameObject);
 		}
 		else if (gameObject.tag == "PlayerBullet" && other.tag == "Player") {
