d3de5607f9b2100bd38a5a838791c48bb3bcd190
unknown <ng125@LLB16-17.chapman.edu>
2014-12-04 19:32:32 -0800

    added scrolling background, title screen

---
+++ Assets/Scripts/MovePointer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePointer : MonoBehaviour {
+	public int numMenuItems = 3;
+	public int menuStartPosition = 1;
+	private int menuCurrPosition;
+	public const int STARTGAME = 1;
+	public const int QUITGAME = 2;
+	// Use this for initialization
+	void Start () {
+		menuCurrPosition = menuStartPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.W) && menuCurrPosition > 1){
+			transform.Translate(0.0f,1.6f,0.0f);
+			menuCurrPosition--;
+		}
+		if (Input.GetKeyDown (KeyCode.S) && menuCurrPosition < numMenuItems) {
+			transform.Translate(0.0f,-1.6f,0.0f);
+			menuCurrPosition++;
+
+		}
+		if (Input.GetKeyDown (KeyCode.Return)) {
+			switch(menuCurrPosition)
+			{
+			case STARTGAME:
+				Application.LoadLevel(1);
+				break;
+			case QUITGAME:
+				Application.Quit();
+				break;
+			default:
+				break;
+
+			}
+
+				}
+	}
+}
---
+++ Assets/Scripts/NewStartMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewStartMenu : MonoBehaviour {
+
+	public GameObject pointer;
+	public float xCordinate;
+	public float yCordinate;
+	private GameObject	tmpPointer;
+	private Vector3 pointerPosition;
+	//private float currMenuPosition;
+	private Quaternion defaultQuat;
+	// Use this for initialization
+	void Start () {
+		defaultQuat = new Quaternion (0,0,0,0);
+		//currMenuPosition = 1;
+		pointerPosition = new Vector3 (xCordinate,yCordinate,0);
+		tmpPointer = (GameObject) Instantiate (pointer,pointerPosition,defaultQuat);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if (Input.GetKeyDown (KeyCode.A)) {
+//			Destroy(tmpPointer);
+//			
+//				}
+//		if(Input.GetKeyDown (KeyCode.UpArrow) ){
+//			Destroy(tmpPointer);
+//			
+//			currMenuPosition +=1;
+//			pointerPosition = new Vector3 (0,currMenuPosition,0);
+//			tmpPointer = (GameObject) Instantiate (pointer,pointerPosition, defaultQuat);
+//			//Destroy (tmpPointer);
+//		}
+	}
+}
